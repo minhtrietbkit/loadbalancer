8835936172e0ba163690045b68c4336e950edace
joe <minhtrietbkit@gmail.com>
2025-03-28 15:21:48 -0400

    refactor receive function

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <netinet/tcp.h>
 
 int select_next_upstream_index(int array_size, int last_used_upstream_index) {
   //Round robin
@@ -29,27 +31,52 @@ void connect_upstream (int local_socket, int upstream_port) {
   }
 }
 
+// if is_blocking != 1, blocks until receive the 1st message and returns; otherwise blocks until the otherside closes connection
+unsigned int receive_message(int socket_fd, ssize_t* response_buffer, ssize_t response_buffer_size, int is_blocking) {
+  ssize_t temp_buffer[BUFSIZ];
+  int total_recv_bytes = 0;
+  while(1){
+    int recv_bytes = recv(socket_fd, temp_buffer, BUFSIZ, 0);
+    if (recv_bytes < 0) {
+      perror("LB: recv failed");
+      exit(EXIT_FAILURE);
+    }
+    if (recv_bytes == 0) {
+      printf("LB: No more data to receive\n");
+      close(socket_fd);
+      break;
+    }
+    total_recv_bytes += recv_bytes;
+    strcpy((char* )response_buffer, (char* )temp_buffer);
+    if (is_blocking != 1) {
+      break;
+    }
+  }
+  printf("LB: Received %d bytes with the following contents\n", total_recv_bytes);
+  printf("%s\n", (char* )temp_buffer);
+  return total_recv_bytes;
+}
+
 unsigned int forward_message_upstream(int upstream_port, ssize_t* req_buffer, unsigned int bytes_to_send, ssize_t* res_buffer, unsigned int res_buffer_size) {
-  //Connect to upstream
-  //send message
-  //recv Response
   int child_socket = create_socket();
+  int bytes_received = 0;
+  //Connect to upstream
   connect_upstream(child_socket, upstream_port);
+
+  //send message
   if (send(child_socket, req_buffer, bytes_to_send, 0) < 0) {
     perror("LB: Send upstream failed");
     exit(EXIT_FAILURE);
   }
-  int recv_return = recv(child_socket, res_buffer, res_buffer_size, 0);
-  //printf("LB: Recv upstream returned %d\n", recv_return);
-  if (recv_return < 0) {
-    perror("LB: Recv upstream failed");
-    exit(EXIT_FAILURE);
-  }
-  if (recv_return == 0) {
-    printf("LB: Upstream connection closed\n");
-    close(child_socket);
-  }
-  return recv_return;
+
+  //recv Response
+  bytes_received = receive_message(child_socket, res_buffer, res_buffer_size, 1);
+  return bytes_received;
+}
+
+int handle_downstream_connection(int child_socket, struct sockaddr_in client_addr) {
+
+  return 0;
 }
 
 void main(int argc, char **argv) {
@@ -79,7 +106,7 @@ void main(int argc, char **argv) {
     // Define server sockaddr_in
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
     int last_used_upstream_index = 0;
     ssize_t req_buffer[BUFSIZ], res_buffer[BUFSIZ];
@@ -102,31 +129,28 @@ void main(int argc, char **argv) {
        perror("LB: Accept downstream connection failed");
        exit(EXIT_FAILURE);
      }
+     pid_t child_pid = fork();
+     if (child_pid < 0) {
+       perror("LB: Fork child process to handle TCP Connection failed");
+     } else if (child_pid == 0) {
+       printf("Child PID %d\n", getpid());
+       int bytes_received = receive_message(child_fd, req_buffer, BUFSIZ, 0);
 
-     int recv_return;
-     recv_return = recv(child_fd, req_buffer, BUFSIZ, 0);
-     if (recv_return < 0) {
-       perror("LB: Receive from downstream failed");
-       exit(EXIT_FAILURE);
-     }
-     if (recv_return == 0) {
-       printf("LB: Downstream closed connection");
+       last_used_upstream_index = select_next_upstream_index(UPSTREAM_COUNT, last_used_upstream_index);
+       // Forward message and get the response
+       unsigned int bytes_received_from_upstream;
+       bytes_received_from_upstream = forward_message_upstream(upstream_ports[last_used_upstream_index], req_buffer, bytes_received, res_buffer, BUFSIZ);
+       //printf("LB: Received %d bytes from upstream\n", bytes_received_from_upstream);
+       // Forward response back
+       if (send(child_fd, res_buffer, bytes_received_from_upstream, 0) < 0){
+         perror("LB: Send upstream failed");
+         exit(EXIT_FAILURE);
+       }
+       shutdown(child_fd, SHUT_WR);
        close(child_fd);
+     } else {
+       printf("LB: Continue to accept other connection\n");
      }
-     printf("LB: Received %d bytes from downstream\n", recv_return);
-
-     last_used_upstream_index = select_next_upstream_index(UPSTREAM_COUNT, last_used_upstream_index);
-     // Forward message and get the response
-     unsigned int bytes_received_from_upstream;
-     bytes_received_from_upstream = forward_message_upstream(upstream_ports[last_used_upstream_index], req_buffer, recv_return, res_buffer, BUFSIZ);
-     //printf("LB: Received %d bytes from upstream\n", bytes_received_from_upstream);
-     // Forward response back
-     if (send(child_fd, res_buffer, bytes_received_from_upstream, 0) < 0){
-       perror("LB: Send upstream failed");
-       exit(EXIT_FAILURE);
-     }
-     close(child_fd);
-
    }
    exit(0);
 }
